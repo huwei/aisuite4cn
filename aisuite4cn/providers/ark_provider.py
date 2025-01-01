61f8b062b53949a7348989741cff586060ef7d49
胡伟 <huwei1@guahao.com>
2025-01-02 00:19:30 +0800

    ark add model mapping

--- aisuite4cn/providers/ark_provider.py
+++ aisuite4cn/providers/ark_provider.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentError
+from urllib.parse import parse_qs
 
 import openai
 import os
@@ -17,10 +18,13 @@ class ArkProvider(Provider):
         # Ensure API key is provided either in config or via environment variable
         self.config = dict(config)
         self.api_key = self.config.pop("api_key", None) or os.getenv("ARK_API_KEY")
+        self.model_map:dict = self.config.get("model_map", None)
+        env_dict = parse_qs(os.getenv("ARK_MODEL_MAP", ""))
+        self.model_map = {k: v[0] for k, v in env_dict.items()}
 
         if not self.api_key:
             raise ValueError(
-                "Ark API key is missing. Please provide it in the config or set the OPENAI_API_KEY environment variable."
+                "Ark API key is missing. Please provide it in the config or set the ARK_API_KEY environment variable."
             )
         # Pass the entire config to the Ark client constructor
 
@@ -35,9 +39,13 @@ class ArkProvider(Provider):
         # Maybe we should catch them and raise a custom LLMError.
 
         if not model.startswith("ep-"):
-            raise ValueError("The model name must be the endpoint ID of the model.")
+            real_model = self.model_map.get(model, None)
+            if real_model is None:
+                raise ValueError("The model name must be the endpoint ID of the model.")
+        else:
+            real_model = model
         return self.client.chat.completions.create(
-            model=model,
+            model=real_model,
             messages=messages,
             **kwargs  # Pass any additional arguments to the Ark API
         )
