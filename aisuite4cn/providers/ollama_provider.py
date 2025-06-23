13a9c2dfce2b000943245fbe02d2397388178b89
hu77wei <hu77wei@gmail.com>
2025-06-23 10:18:23 +0800

    fix(ollama_provider): 支持环境变量设置 Ollama API Key
    - 在 OllamaProvider 类中添加对环境变量 OLLAMA_API_KEY 的支持
    - 更新版本号至 0.1.16

--- aisuite4cn/providers/ollama_provider.py
+++ aisuite4cn/providers/ollama_provider.py
@@ -14,10 +14,9 @@ class OllamaProvider(Provider):
         """
 
         self.config = dict(config)
-        self.config['api_key'] = "ollama"
 
         self.base_url = self.config.pop("base_url", os.getenv("OLLAMA_BASE_URL"))
-        self.config['api_key'] = self.config.get('api_key', "ollama")
+        self.config['api_key'] = self.config.get('api_key', os.getenv("OLLAMA_API_KEY",  "ollama"))
         # Pass the entire config to the Ollama client constructor
         self.client = openai.OpenAI(
             base_url=self.base_url,
