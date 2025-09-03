0e51a01b46fde499a86307c747d54fd9347f0856
hu77wei <hu77wei@gmail.com>
2025-09-04 00:12:25 +0800

    feat(llama_index): 添加 LlamaIndexClient 实现
    - 新增 LlamaIndexClient 类，用于与 DeepSeek 等服务进行交互
    - 实现了聊天、完成、结构化预测等功能
    - 添加了异步和同步两种调用方式
    - 集成了工具调用和流式处理支持

---
+++ examples/llama_index/deepseek_example.py
@@ -0,0 +1,37 @@
+
+from dotenv import load_dotenv
+from aisuite4cn.llama_index import LlamaIndexClient
+
+# 加载 .env 文件中的环境变量
+load_dotenv()
+
+provider = "deepseek"
+model_id = "deepseek-reasoner"
+# model_id = "deepseek-chat"
+
+
+client = LlamaIndexClient()
+
+
+# resp = client.complete(model=f"{provider}:{model_id}", prompt="甲基丙烯酰化明胶的合成方案")
+#
+# print(resp.text)
+
+
+resp = client.stream_complete(model=f"{provider}:{model_id}", prompt="甲基丙烯酰化明胶的合成方案")
+for chunk in resp:
+    if chunk.raw.choices[0].delta.reasoning_content:
+        print(chunk.raw.choices[0].delta.reasoning_content, end='')
+    else:
+        print(chunk.delta, end='')
+
+#
+# import asyncio
+# # 方案1: 使用异步函数包装
+# async def async_call():
+#     resp = await client.acomplete(model="{provider}:{model_id}", prompt="根据用户的问题，输出回答内容，内容返回一个json字符串，包含answer字段")
+#     print(resp.text)
+#     return resp
+#
+# # 运行异步函数
+# asyncio.run(async_call())
