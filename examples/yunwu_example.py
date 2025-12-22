bf49603a2201e15fc1ac8b5ae46c3d2eec9e1d51
hu77wei <hu77wei@gmail.com>
2025-12-22 08:49:51 +0800

    feat(yunwu): 添加云雾AI服务支持

---
+++ examples/yunwu_example.py
@@ -0,0 +1,45 @@
+from dotenv import load_dotenv
+
+import aisuite4cn as ai
+
+# 加载 .env 文件中的环境变量
+load_dotenv()
+
+client = ai.Client()
+
+provider = "yunwu"
+# model_id = "deepseek-reasoner"
+model_id = "deepseek-chat"
+
+messages = [
+    {"role": "system", "content": "根据用户的问题，输出回答内容，内容返回一个json字符串，包含answer字段"},
+    {"role": "user", "content": "who are you"},
+]
+
+# stream = True
+response = client.chat.completions.create(
+    model=f"{provider}:{model_id}",
+    messages=messages,
+    stream=True,
+    response_format={
+        "type": "json_object"
+    }
+)
+
+for chunk in response:
+
+    if chunk.choices[0].delta.content:
+        print(chunk.choices[0].delta.content, end='')
+
+# stream = False
+#
+# response = client.chat.completions.create(
+#     model=f"{provider}:{model_id}",
+#     messages=messages,
+#     stream=False,
+#     response_format={
+#         "type": "json_object"
+#     }
+# )
+#
+# print(response.choices[0].message.content)
